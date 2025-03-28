037aaf1abc12c8e748f2f3033bb863ffd057e89b
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-28 10:23:02 +0000

    Completed first part of chapter 2, moving onto the 'Generating a secret number' section. Also added some annotations to the current code.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number!"); // these run a macro that outputs what we put in the parentheses into the console
     
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); // Variables in rust are immutable by default, hence why we specifed 'mut'
+    // The line above also binds String to the return of function 'String' which creates a new instance of String. It's a standard library that is growable.
+    // the :: means that new is an associated function of the String type.
+    // Associated functions are those that are implemented on a type like in this case String. This creates a new empty string.
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    // We also could've just done std::io::stdin() below and it would work.
+    io::stdin()// We're calling the 'stdin()' function from our previously imported 'std::io', it allows us to handler user input.
+        .read_line(&mut guess) // calls the 'read_line' method on the standard input handle to get input from the user
+        // we also pass &mut guess as an argument to tell the string to store the user input into it. The '&' symbol indicates a reference.
+        .expect("Failed to read line"); // This handles any potensial errors by throwing a fail message if the enum returns a err instead of ok.
     
-    println!("You guessed: {guess}");
+    println!("You guessed: {guess}"); // Lastly we print the vlaue using the guess placeholder within curly braces {}. Quite similar to React for example...
 }
